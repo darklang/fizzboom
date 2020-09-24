3face5458a3a542a4d896c05c5f69c222a367c1a
Paul Biggar <paul.biggar@gmail.com>
2020-09-24 16:52:49 -0400

    Make delay.js configurable

--- delay.js
+++ delay.js
@@ -2,12 +2,20 @@ const http = require("http");
 
 const server = http.createServer();
 
-server.on("request", async (req, res) => {
-  await new Promise((r) => setTimeout(r, 1000));
-  let response = '{"data": ""}';
-  res.writeHead(200, { "content-length": response.length });
-  res.write(response);
-  res.end();
+server.on("request", async (message, res) => {
+  let path = message.url.split("/");
+  if (path[1] == "delay") {
+    let time = parseInt(path[2]);
+    await new Promise((r) => setTimeout(r, time * 1000));
+    let response = '{"data": ""}';
+    res.writeHead(200, { "content-length": response.length });
+    res.write(response);
+    res.end();
+  } else {
+    res.writeHead(404, {});
+    res.write("path should be /delay/{number of seconds}");
+    res.end();
+  }
 });
 
 server.listen(1025);
