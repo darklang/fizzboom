a8edecfe9a7da1ab605b40bab5f040cd447f4fac
Paul Biggar <paul.biggar@gmail.com>
2020-09-24 14:26:04 -0400

    Add fizzboom for rust-hyper

--- rust-hyper/backend/src/main.rs
+++ rust-hyper/backend/src/main.rs
@@ -4,13 +4,56 @@
 
 use hyper::{
   service::{make_service_fn, service_fn},
-  Body, Request, Response, Server,
+  Body, Request, Response, Server, Method, StatusCode
 };
 use std::{convert::Infallible, net::SocketAddr};
 
 use execution_engine::{self, eval, expr::*, ivec, runtime};
 
-async fn program() -> Expr {
+fn fizzboom() -> Expr {
+  elet("range",
+       esfn("Int", "range", 0, ivec![eint(1), eint(100),]),
+       esfn("List",
+            "map",
+            0,
+            ivec![(evar("range")),
+                  elambda(ivec!["i"],
+                          eif(ebinop(ebinop(evar("i"),
+                                            "Int",
+                                            "%",
+                                            0,
+                                            eint(15)),
+                                     "Int",
+                                     "==",
+                                     0,
+                                     eint(0)),
+                              esfn("HTTPClient", "get", 0, ivec![estr("http://localhost:1025/delay/1")]),
+                              eif(ebinop(ebinop(evar("i"),
+                                                "Int",
+                                                "%",
+                                                0,
+                                                eint(5)),
+                                         "Int",
+                                         "==",
+                                         0,
+                                         eint(0)),
+                                  estr("buzz"),
+                                  eif(ebinop(ebinop(evar("i"),
+                                                    "Int",
+                                                    "%",
+                                                    0,
+                                                    eint(3)),
+                                             "Int",
+                                             "==",
+                                             0,
+                                             eint(0)),
+                                      estr("fizz"),
+                                      esfn("Int",
+                                           "toString",
+                                           0,
+                                           ivec![evar("i")])))))]))
+}
+fn fizzbuzz() -> Expr {
   elet("range",
        esfn("Int", "range", 0, ivec![eint(1), eint(100),]),
        esfn("List",
@@ -54,17 +97,31 @@ async fn program() -> Expr {
                                            ivec![evar("i")])))))]))
 }
 
-async fn run_program(_req: Request<Body>)
-                     -> Result<Response<Body>, Infallible> {
+async fn run_program(program: Expr)
+                     -> String {
   let tlid = runtime::TLID::TLID(7);
   let state =
     eval::ExecState { caller: runtime::Caller::Toplevel(tlid), };
-  let result;
-  {
-    let prog = program().await;
-    result = eval::run_json(&state, prog);
-  }
-  Ok(Response::new(result.to_string().into()))
+  let result = eval::run_json(&state, program);
+  result.to_string()
+}
+
+async fn handle(req: Request<Body>)
+                     -> Result<Response<Body>, Infallible> {
+  let mut response = Response::new(Body::empty());
+
+  match (req.method(), req.uri().path()) {
+      (&Method::GET, "/fizzbuzz") => {
+          *response.body_mut() = Body::from(run_program(fizzbuzz()).await);
+      },
+      (&Method::GET, "/fizzboom") => {
+          *response.body_mut() = Body::from(run_program(fizzboom()).await);
+      },
+      _ => {
+          *response.status_mut() = StatusCode::NOT_FOUND;
+      },
+  };
+  Ok(response)
 }
 
 #[tokio::main]
@@ -78,7 +135,7 @@ async fn main() {
   // creates one from our `hello_world` function.
   let make_svc = make_service_fn(|_conn| async {
     // service_fn converts our function into a `Service`
-    Ok::<_, Infallible>(service_fn(run_program))
+    Ok::<_, Infallible>(service_fn(handle))
   });
 
   let server = Server::bind(&addr).serve(make_svc);
